6dd137ba778294fa1ec34956af3d41c0e631d47c
Lakshmi Shaji <lakshnair45@gmail.com>
2020-11-10 18:06:05 -0500

    Add files via upload

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Schmoceries", Price = 42 },
+            new Product { Id = 2, Name = "Potato Soup", Category = "Schmoceries", Price = 42 },
+            new Product { Id = 3, Name = "Potato Soup", Category = "Schmoceries", Price = 42 }
         };
 
         public ProductsController()
